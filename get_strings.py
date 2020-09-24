afd98d19130b0eeacb0da17f8e5ca1bd2534fa94
k1rh4 <k1rh4.lee@gmail.com>
2020-09-23 22:06:43 -0500

    Change analyzer api

--- get_strings.py
+++ get_strings.py
@@ -1,22 +1,17 @@
 import sys
 
-#from androguard.core.bytecodes.apk import APK
-#from androguard.core.bytecodes.dvm import DalvikVMFormat
-#from androguard.core.bytecodes.dvm import ClassDefItem
+from androguard.core.bytecodes.apk import APK
+from androguard.core.bytecodes.dvm import DalvikVMFormat
+from androguard.core.bytecodes.dvm import ClassDefItem
 
-from androguard.misc import AnalyzeAPK
 
 def print_k(msg, info='+'):
         print("[{}] {}".format(info,msg))
 
 def usage():
-    print_k("python ./{} [apk_file]".format(sys.argv[0]))
+    print_k("python {} [apk_file]".format(sys.argv[0]))
 
-def find_function_call(method_, key_func="/"):
-    if method_.is_external():
-        return 0
-    method = method_.get_method()
-
+def find_function_call(method, key_func="/"):
     if not method.get_code(): return 0
     instruction_tmp = []
     for instruction in method.get_instructions():
@@ -31,21 +26,13 @@ def find_function_call(method_, key_func="/"):
 
 def init(apk_file):
     print_k(apk_file)
-    try:
-        pass
-        #dalvik_ctx = DalvikVMFormat( APK(apk_file) )
-    except :
-        print_k("DalvicVMFormat Error","-")
-        sys.exit();
-    a,d,dx = AnalyzeAPK(apk_file)
-    methods = dx.get_methods()
-    #methods = dalvik_ctx.get_methods()
-    #class = "com.oppo.gallery3d.o.a"
+    dalvik_ctx = DalvikVMFormat( APK(apk_file) )
+    methods = dalvik_ctx.get_methods()
     return methods 
 
 def print_l(list_data):
     for msg in list_data :
-        print ("\t{}".format(msg))
+        print "\t{}".format(msg)
 
 def scan_methods(methods, key_func="/"):
     for method in methods:
@@ -53,7 +40,7 @@ def scan_methods(methods, key_func="/"):
         if inst_list :
             print_k(method,"->")
             print_l(inst_list)
-                     
+                        
 def main(): 
     if(len(sys.argv) > 1):
         apk_name = sys.argv[1]
